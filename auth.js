dd3060e110f354cfce7d11e10d0d11a2dba7f677
Lexarti3 <Leharti@yandex.ru>
2026-01-30 15:32:02 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,22 +1,11 @@
+<script type="module">
+import { auth } from "./firebase.js";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signOut
+  signOut,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
-
-const auth = window.auth;
-
-window.register = async () => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  try {
-    await createUserWithEmailAndPassword(auth, email, password);
-    alert("Регистрация успешна");
-  } catch (e) {
-    alert(e.message);
-  }
-};
 
 window.login = async () => {
   const email = document.getElementById("email").value;
@@ -30,6 +19,33 @@ window.login = async () => {
   }
 };
 
+window.register = async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("Аккаунт создан");
+  } catch (e) {
+    alert(e.message);
+  }
+};
+
 window.logout = async () => {
   await signOut(auth);
 };
+
+onAuthStateChanged(auth, user => {
+  const status = document.getElementById("auth-status");
+  const logoutBtn = document.getElementById("logout-btn");
+
+  if (user) {
+    status.innerText = "Вы вошли: " + user.email;
+    logoutBtn.style.display = "block";
+  } else {
+    status.innerText = "Вы не авторизованы";
+    logoutBtn.style.display = "none";
+  }
+});
+</script>
+
